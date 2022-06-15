252eabc95b56d67abff9c4d344e01eb84052cae5
berti <berti@CLASSES.LOCAL>
2022-06-15 09:58:02 +0200

    delete works

--- admin/works.php
+++ admin/works.php
@@ -7,6 +7,29 @@
 
     require "../connexion.php";
 
+    if(isset($_GET['delete']))
+    {
+        $id= htmlspecialchars($_GET['delete']);
+        $reqWorks = $bdd->prepare("SELECT * FROM works WHERE id=?");
+        $reqWorks->execute([$id]);
+        if(!$donMyWorks = $reqWorks->fetch())
+        {
+            $reqWorks->closeCursor();
+            header("LOCATION:works.php");
+        }
+        $reqWorks->closeCursor();
+
+        unlink("../images/".$donMyWorks['cover']);
+
+        $delete = $bdd->prepare("DELETE FROM works WHERE id=?");
+        $delete->execute([$id]);
+        $delete->closeCursor();
+        
+        header("LOCATION:works.php?deletesuccess=".$id);
+
+
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -34,6 +57,10 @@
                   if(isset($_GET['update']) && isset($_GET['id']))
                   {
                       echo "<div class='alert alert-warning'>Vous avez bien modifié l'oeuvre n°".$_GET['id']."</div>";
+                  }
+                  if(isset($_GET['deletesuccess']))
+                  {
+                      echo "<div class='alert alert-danger'>Vous avez bien supprimé l'oeuvre n°".$_GET['deletesuccess']."</div>";
                   }
             ?>
             <a href="worksAdd.php" class='btn btn-success'>Ajouter</a>
